929b065247df3252ab34b62fbb08c934b6418578
Juan Pablo Davalos <davjuanpablo423@gmail.com>
2024-07-03 15:30:42 -0700

    implemented mobile share button interaction

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ const contentFooter = document.querySelector(".content-footer");
 
 const shareButton = document.querySelector(".share-button");
 const shareOptions = document.querySelector(".share-options");
-const authorInfo = document.querySelector(".author-info");
 
 shareButton.onclick = function () {
   if (desktopMedia.matches) {
@@ -16,16 +15,12 @@ shareButton.onclick = function () {
       shareOptions.classList.add("hidden");
     }
   } else {
-    if (authorInfo.classList.contains("hidden")) {
+    if (shareOptions.classList.contains("hidden")) {
       console.log("removing hidden class");
-      //   shareOptions.classList.remove("hidden");
-      // authorInfo.classList.remove("hidden");
-      contentFooter.style.justifyContent = "space-between";
+      shareOptions.classList.remove("hidden");
     } else {
       console.log("adding hidden class");
-      //   shareOptions.classList.add("hidden");
-      // authorInfo.classList.add("hidden");
-      contentFooter.style.justifyContent = "flex-end";
+      shareOptions.classList.add("hidden");
     }
   }
 };
