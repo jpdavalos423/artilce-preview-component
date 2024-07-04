5b6b393ad04fbfe4c9a89dcfabbd7c0ae59b5e7c
Juan Pablo Davalos <davjuanpablo423@gmail.com>
2024-07-03 18:00:51 -0700

    cleaned up js code

--- main.js
+++ main.js
@@ -4,9 +4,7 @@ const shareOptions = document.querySelector(".share-options");
 shareButton.onclick = function () {
   if (shareOptions.classList.contains("hidden")) {
     console.log("removing hidden class");
-    shareOptions.classList.remove("hidden");
   } else {
     console.log("adding hidden class");
-    shareOptions.classList.add("hidden");
   }
 };
