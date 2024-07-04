7d7b190d5227bc7170a246b87c747967a66f657e
Juan Pablo Davalos <davjuanpablo423@gmail.com>
2024-07-03 17:45:28 -0700

    cleaned up unnecessary code

--- main.js
+++ main.js
@@ -1,26 +1,12 @@
-const desktopMedia = window.matchMedia("(min-width: 768px)");
-
-const contentFooter = document.querySelector(".content-footer");
-
 const shareButton = document.querySelector(".share-button");
 const shareOptions = document.querySelector(".share-options");
 
 shareButton.onclick = function () {
-  if (desktopMedia.matches) {
-    if (shareOptions.classList.contains("hidden")) {
-      console.log("removing hidden class");
-      shareOptions.classList.remove("hidden");
-    } else {
-      console.log("adding hidden class");
-      shareOptions.classList.add("hidden");
-    }
+  if (shareOptions.classList.contains("hidden")) {
+    console.log("removing hidden class");
+    shareOptions.classList.remove("hidden");
   } else {
-    if (shareOptions.classList.contains("hidden")) {
-      console.log("removing hidden class");
-      shareOptions.classList.remove("hidden");
-    } else {
-      console.log("adding hidden class");
-      shareOptions.classList.add("hidden");
-    }
+    console.log("adding hidden class");
+    shareOptions.classList.add("hidden");
   }
 };
